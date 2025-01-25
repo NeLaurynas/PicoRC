194cd9c7a2a56f96662c75cfbcb0ce73cc91616f
NeLaurynas <ndisposable@gmail.com>
2025-01-25 22:36:53 +0200

    firetruck project

---
+++ firetruck/defines/config.h
@@ -0,0 +1,16 @@
+// Copyright (C) 2025 Laurynas 'Deviltry' Ekekeke
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DBG true
+
+#define RENDER_TICK 10'000
+
+#define MOD_ENGINE_XY_DEAD_ZONE 40
+#define MOD_ENGINE_XY_MAX 510
+
+
+
+#endif //CONFIG_H
---
+++ firetruck/modules/engine/main_engine.c
@@ -0,0 +1,11 @@
+// Copyright (C) 2025 Laurynas 'Deviltry' Ekekeke
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "main_engine.h"
+
+void main_engine_init() {
+
+}
+
+void main_engine_vals(i16 left, i16 right) {
+}

3dbb53a5f019b661d3428a9fb7efadc151e7e4ba
NeLaurynas <ndisposable@gmail.com>
2025-04-06 22:55:46 +0300

    buggy

--- buggy/main.c
+++ buggy/main.c
@@ -1,42 +1,43 @@
 // Copyright (C) 2025 Laurynas 'Deviltry' Ekekeke
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include <btstack_run_loop.h>
+#include <stdio.h>
 #include <hardware/clocks.h>
 #include <pico/cyw43_arch.h>
 #include <pico/cyw43_driver.h>
-#include <pico/multicore.h>
 #include <pico/stdio.h>
 #include <pico/time.h>
-#include <stdio.h>
-#include <uni.h>
 #include "boards/pico2_w.h"
+#include <btstack_run_loop.h>
+#include <uni.h>
+#include <pico/multicore.h>
 
 #include "renderer.h"
 #include "sdkconfig.h"
-#include "utils.h"
+
+#include "defines/config.h"
 
 #undef PICO_FLASH_ASSERT_ON_UNSAFE
 #define PICO_FLASH_ASSERT_ON_UNSAFE 0
 
-struct uni_platform *get_rc_platform(void);
+struct uni_platform* get_rc_platform(void);
 
 int main() {
-	set_sys_clock_khz(48'000, false);
+	set_sys_clock_khz(18'000, false);
 
 	stdio_init_all();
 
 #if DBG
 	sleep_ms(2000);
-	printf("Slept for 2 seconds\n");
+	printf("Slept for 2 secondss\n");
 #endif
 
+	// initialize CYW43 driver architecture (will enable BT if/because CYW43_ENABLE_BLUETOOTH == 1)
 	if (cyw43_arch_init_with_country(CYW43_COUNTRY_LITHUANIA)) {
 		loge("failed to initialise cyw43_arch\n");
-		utils_error_mode(66);
+		return -1;
 	}
 	cyw43_set_pio_clock_divisor(1, 0);
-
 	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
 	uni_platform_set_custom(get_rc_platform());
 	uni_init(0, NULL);
---
+++ buggy/state.h
@@ -0,0 +1,82 @@
+// Copyright (C) 2025 Laurynas 'Deviltry' Ekekeke
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef STATE_Htypedef uint32_t u32;
+#define STATE_H
+
+#include <shared_config.h>
+
+// ENUMS
+
+typedef struct {
+	i32 x;
+	i32 y;
+
+	i32 rx;
+	i32 ry;
+
+	bool btn_a;
+	bool btn_x;
+	bool btn_b;
+	bool btn_y;
+
+	bool btn_start;
+	bool btn_select;
+
+	bool shoulder_l;
+	bool shoulder_r;
+
+	i32 throttle;
+	i32 brake;
+
+	bool full_beans;
+
+	struct {
+		bool blinkers_left;
+		bool blinkers_right;
+
+		i32 head;
+		i32 tail;
+
+		bool siren;
+	} lights;
+} State;
+
+typedef struct {
+	i32 x;
+	i32 y;
+
+	i32 rx;
+	i32 ry;
+
+	bool btn_a;
+	bool btn_x;
+	bool btn_b;
+	bool btn_y;
+
+	bool shoulder_l;
+	bool shoulder_r;
+
+	bool btn_start;
+	bool btn_select;
+
+	i32 throttle;
+	i32 brake;
+
+	bool full_beans;
+
+	struct {
+		bool blinkers_left;
+		bool blinkers_right;
+
+		i32 head;
+		i32 tail;
+
+		bool siren;
+	} lights;
+} CurrentState;
+
+volatile extern State state;
+volatile extern CurrentState current_state;
+
+#endif //STATE_H
